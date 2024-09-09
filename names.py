e32547558fdf983c8a8a6630767d6b750dd51c68
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-09 16:36:56 +0300

    docs

--- names.py
+++ names.py
@@ -3,6 +3,12 @@ from turtle import Turtle
 class Names:
 
     def write_name(self, location: tuple, user_guess: str):
+        """_Create a turtle and move it to given location, writing its name _
+
+        Args:
+            location (tuple): _Location of the state_
+            user_guess (str): _Name of state given by user_
+        """
         turtle = Turtle()
         turtle.penup()
         turtle.hideturtle()
