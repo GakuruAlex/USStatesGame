263e7c62c7d59b66b37a06789d278cdc5933368a
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-09 16:34:52 +0300

    setup screen and import the various classes

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from turtle import Screen,Turtle
-
+from u_s_states import USStates
+from names import Names
 def main()-> None:
     screen = Screen()
     turtle = Turtle()
+    u_s_states = USStates()
+    name = Names()
+    game_is_on = True
+
     screen.title("U.S. States Game")
     image = "blank_states_img.gif"
-    screen.addshape(image)
+    screen.register_shape(image)
     turtle.shape(image)
-    
+    counter = 0
+    while game_is_on:
+        u_s_states.print_data()
+        no_states = len(u_s_states.data)
+        user_answer = screen.textinput(title=f"{counter}/{no_states}",prompt="Enter a state: ")
+        if u_s_states.check_state(user_answer):
+            location = u_s_states.location_of_state(user_answer)
+            name.write_name(location=location, user_guess=user_answer)
+            counter += 1
+
+
     screen.exitonclick()
 
 if __name__ == "__main__":
