3cf6b2fd233e64595b54d9bb0894dd328639df91
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-09 14:47:30 +0300

    Create turtle screen and add image

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from turtle import Screen
+from turtle import Screen,Turtle
+
+def main()-> None:
+    screen = Screen()
+    turtle = Turtle()
+    screen.title("U.S. States Game")
+    image = "blank_states_img.gif"
+    screen.addshape(image)
+    turtle.shape(image)
+    
+    screen.exitonclick()
+
+if __name__ == "__main__":
+    main()
